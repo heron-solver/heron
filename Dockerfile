2bcf670c51a53a3a5b521e0a616dda2beb61bb1b
Hai Nguyen Van <hai.nguyen-van@lri.fr>
2020-04-15 11:15:32 +0200

    Docker MOTD

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM shwestrick/mpl
 WORKDIR /heron
+COPY . /heron
 RUN apt install -y --force-yes time
-RUN git clone https://github.com/heron-solver/heron .
+# RUN git clone https://github.com/heron-solver/heron .
 RUN make
 RUN chmod +x /heron/lib/docker-motd.sh
 RUN echo "/heron/lib/docker-motd.sh" >> ~/.bashrc
+RUN mkdir ~/.ssh
+RUN echo "Host github.com" >> ~/.ssh/config
+RUN echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 ENV PATH="/heron:${PATH}"
--- lib/docker-motd.sh
+++ lib/docker-motd.sh
@@ -6,17 +6,17 @@ HERON_VER=$(if [ -z "$HERON_VER" ] ; then printf "(not found)          " ; else 
 CPUS=$(lscpu | egrep 'Model name|Socket|Thread|NUMA|CPU\(s\)' | head -n 1 | cut -d ':' -f 2 | awk 'gsub(/^[ \t]+/,"")')
 HOSTNAME=$(hostname)
 
-BGN1=$(tput setab 23)
-BGN2=$(tput setab 8)
+BGN1=$(tput setab 4)
+BGN2=$(tput setab 5)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 BOLD=$(tput bold)
 
 # Graphics
 printf "$BGN1                                   $RESET"                     ; printf "  ${BGN2}                                       $RESET\n" 
-printf "$BGN1$BOLD  Heron                            $RESET"		      ; printf "  ${BGN2}  For assistance, just type:           $RESET\n" 
+printf "$BGN1$BOLD  Heron                            $RESET"		      ; printf "  ${BGN2}$BOLD  For assistance, just type:           $RESET\n" 
 printf "$BGN1$BOLD  Version: $HERON_VER   $RESET"                           ; printf "  ${BGN2}$BOLD    heron --help                       $RESET\n" 
-printf "$BGN1$BOLD  Docker:  $HOSTNAME            $RESET"		      ; printf "  ${BGN2}  To run some examples                 $RESET\n" 
+printf "$BGN1$BOLD  Docker:  $HOSTNAME            $RESET"		      ; printf "  ${BGN2}$BOLD  To run some examples                 $RESET\n" 
 printf "$BGN1$BOLD  CPU(s):  %02d                      $RESET" $CPUS	      ; printf "  ${BGN2}$BOLD    heron --use examples/[TESL FILE]   $RESET\n" 
 printf "$BGN1                                   $RESET"  		      ; printf "  ${BGN2}                                       $RESET\n\n" 
 
